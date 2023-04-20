bc4cd6d27e0f909e42649b357ebc2eec6f5930c0
wanderfranca <wander.wpf@hotmail.com>
2023-04-19 23:26:02 -0300

    Seção 8: Aula 102

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,10 +3,14 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
-  @Input() titulo : string | undefined;
+  @Input() titulo: string | undefined;
+  @Input() iconClass: string | undefined;
+  @Input() subtitulo: 'Desde 2023' = 'Desde 2023';
+  @Input() botaoListar = false;
+
   constructor() { }
 
   ngOnInit(): void {
