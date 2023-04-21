9e5b3d457ded2e73c53978d47359880e0d73f648
wanderfranca <wander.wpf@hotmail.com>
2023-04-20 21:35:56 -0300

    Seção 8: Aula 108

--- Front/ProEventos-App/src/app/app-routing.module.ts
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
 import { PerfilComponent } from './components/perfil/perfil.component';
 
@@ -13,7 +11,6 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent },
   { path: 'contatos', component: ContatosComponent },
   { path: 'palestrantes', component: PalestrantesComponent },
-  { path: 'perfil', component: PerfilComponent },
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full' }
 
