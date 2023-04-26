827d291f025e22b11838ebc25befd7d46f9aaec8
wanderfranca <wander.wpf@hotmail.com>
2023-04-25 21:42:15 -0300

    Seção 8 - finalizado

--- Front/ProEventos-App/src/app/app.module.ts
+++ Front/ProEventos-App/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
@@ -54,6 +54,7 @@ import { RegistrarComponent } from './components/user/registrar/registrar.compon
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
--- Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
   templateUrl: './evento-detalhe.component.html',
-  styleUrls: ['./evento-detalhe.component.css']
+  styleUrls: ['./evento-detalhe.component.scss']
 })
 export class EventoDetalheComponent implements OnInit {
 
+  form!: FormGroup;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.validation();
+  }
+
+  public validation(): void {
+    this.form = new FormGroup({
+
+      tema: new FormControl('',
+
+      [Validators.required, Validators.minLength(4), Validators.maxLength(50)]),
+
+      local: new FormControl('', Validators.required),
+
+      dataEvento: new FormControl('', Validators.required),
+
+      qtdPessoas: new FormControl('',
+      [Validators.required, Validators.max(120000)]
+      ),
+      telefone: new FormControl('', Validators.required),
+
+      email: new FormControl('',
+      [Validators.required, Validators.email]),
+      imagemURL: new FormControl('', Validators.required),
+    });
   }
 
 }
