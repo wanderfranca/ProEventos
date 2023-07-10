00f574344449db97eb09408133fa1687ce7ff7f4
wanderfranca <wander.wpf@hotmail.com>
2023-07-10 18:30:12 -0300

    Upds gerais antes de mexer em tudo

--- Back/src/ProEventos.Domain/Palestrante.cs
+++ Back/src/ProEventos.Domain/Palestrante.cs
@@ -3,7 +3,6 @@ namespace ProEventos.Domain
     public class Palestrante
     {
         public int Id { get; set; }
-        public string Nome { get; set; }
         public string MiniCurriculo { get; set; }
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
